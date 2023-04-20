6a4e9e3dd9b5df4f89ad05ad039ae65a449a853e
iOllaph <pabllo164164@gmail.com>
2023-04-19 23:43:55 -0300

    Added functions to Categories Nav

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,115 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+
+  subcategories = "none";
+
+  lightingExpanded = false;
+  lightingNormal = true;
+  navLighting = "east"
+
+  diningSetsExpanded = false;
+  diningSetsNormal = true;
+  navDiningSets = "east"
+
+  chairsExpanded = false;
+  chairsNormal = true;
+  navChairs = "east"
+
+
+  storageExpanded = false;
+  storageNormal = true;
+  navStorage = "east"
+
+  constructor() { };
+
+  ngOnInit(): void { };
+
+
+  toggleCategoriesRow(index: number) {
+
+    if (index == 0) {
+
+      this.lightingExpanded = !this.lightingExpanded;
+      this.lightingNormal = !this.lightingNormal;
+
+      if (this.lightingExpanded == true) {
+
+        return this.navLighting = "south"
+
+      } else {
+
+        return this.navLighting = "east"
+
+      }
+
+
+    } else if (index == 1) {
+
+      this.diningSetsExpanded = !this.diningSetsExpanded;
+      this.diningSetsNormal = !this.diningSetsNormal;
+
+      if (this.diningSetsExpanded == true) {
+
+        return this.navDiningSets = "south"
+
+      } else {
+
+        return this.navDiningSets = "east"
+
+      }
+
+
+
+    } else if (index == 2) {
+
+      this.chairsExpanded = !this.chairsExpanded;
+      this.chairsNormal = !this.chairsNormal;
+
+
+      if (this.chairsExpanded == true) {
+
+        return this.navChairs = "south"
+
+      } else {
+
+        return this.navChairs = "east"
+
+      }
+
+
+
+    } else if (index == 3) {
+
+      this.storageExpanded = !this.storageExpanded;
+      this.storageNormal = !this.storageNormal;
+
+
+      if (this.storageExpanded == true) {
+
+        return this.navStorage = "south"
+
+      } else {
+
+        return this.navStorage = "east"
+
+      }
+
+
+
+    }
+  }
+
+  checkRowStatus(index: number) {
+
+
+
+
+
+
+
+
+  }
+
+
 }
