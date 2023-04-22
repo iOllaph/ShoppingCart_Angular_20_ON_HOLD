553e71e1b37a834f07a6399c9f2ad9ffb3a931a6
iOllaph <pabllo164164@gmail.com>
2023-04-22 13:11:01 -0300

    Minor changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { ComponentFixture } from '@angular/core/testing';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { SubCategoriesCollectionsComponent } from './sub-categories-collections/sub-categories-collections.component';
 
 
 const routes: Routes = [
 
   { path: "", component: HomeComponent },
-
+  { path: "categories", component: CategoriesComponent },
+  { path: "sub-categorie-collections", component: SubCategoriesCollectionsComponent }
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,37 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LatestCollectionComponent } from './home/latest-collection/latest-collection.component';
 import { BrandsComponent } from './home/brands/brands.component';
+import { AboutUsComponent } from './home/about-us/about-us.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { SubCategoriesComponent } from './categories/sub-categories/sub-categories.component';
+import { FooterComponent } from './footer/footer.component';
+import { SubCategoriesCollectionsComponent } from './sub-categories-collections/sub-categories-collections.component';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LatestCollectionComponent,
-    BrandsComponent
+    BrandsComponent,
+    AboutUsComponent,
+    CategoriesComponent,
+    SubCategoriesComponent,
+    FooterComponent,
+    SubCategoriesCollectionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
