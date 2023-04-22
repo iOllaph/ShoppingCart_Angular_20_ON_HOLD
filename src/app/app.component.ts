a98f5155034d645694a9de34259bc56c06affa83
iOllaph <pabllo164164@gmail.com>
2023-04-22 13:11:38 -0300

    Added a custom Nav-bar for sub-categories-colllections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ShoppingCart_Angular';
+  subcategories = "none";
+  navbarStatus = "grid";
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const url = this.router.url;
+        if (url === '/sub-categorie-collections') {
+          this.subcategories = "block";
+          this.navbarStatus = "none";
+        } else {
+          this.subcategories = "none";
+          this.navbarStatus = "grid";
+        }
+      }
+    });
+  }
+
 }
